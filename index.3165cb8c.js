e93c7f0d377fe60cfbe97a4239f0ee16486564ae
Mykyta Forostovets <122729163+Galenwood@users.noreply.github.com>
2024-09-22 10:46:46 +0300

    make build

---
+++ index.3165cb8c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall");document.querySelector(".spider").style.position="inherit",e.style.display="flex",e.style.alignItems="center",e.style.justifyContent="center";
+//# sourceMappingURL=index.3165cb8c.js.map
